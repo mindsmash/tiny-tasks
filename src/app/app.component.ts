50cc23f51adc84a1e9694469ceab296bbf82ae36
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2018-09-12 14:45:57 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tiny-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tiny-tasks';
+}
