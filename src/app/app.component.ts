bbb9e6218ece97f2b6dc22a3bdf1e583dd53e798
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2019-01-09 14:32:59 +0100

    Added documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,28 @@ export class AppComponent {
 
   tasks: Array<string> = [];
 
-  add(task: string) {
+  /**
+   * Adds a new task to the list of tasks.
+   *
+   * @param task the task's description
+   */
+  add(task: string): void {
     this.tasks.push(task);
   }
 
-  remove(index: number) {
+  /**
+   * Removes the task with the given index from the list of tasks.
+   *
+   * @param index the index of the task to be removed
+   */
+  remove(index: number): void {
     this.tasks.splice(index, 1);
   }
 
-  clear() {
+  /**
+   * Clears the list of tasks.
+   */
+  clear(): void {
     this.tasks.splice(0);
   }
 }
