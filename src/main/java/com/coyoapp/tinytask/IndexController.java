8599e0e566f8d5d8150925559f7d85d634a5e1ad
Bernd Hoffmann <bernd.hoffmann@coyoapp.com>
2019-03-19 16:21:24 +0100

    setup backend scaffold

---
+++ src/main/java/com/coyoapp/tinytask/IndexController.java
@@ -0,0 +1,16 @@
+package com.coyoapp.tinytask;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class IndexController {
+
+  @GetMapping
+  public String getIndexContent() {
+    return "Tiny Task Server is up and running.";
+  }
+
+}
