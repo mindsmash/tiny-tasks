55ea6754c5ba324bb13daa6d2e633ef163e10491
Nadeem Jamali <nadeemjamali337@gmail.com>
2019-07-22 12:45:29 +0200

    added app-user entity

---
+++ src/main/java/com/coyoapp/tinytask/domain/AppUser.java
@@ -0,0 +1,29 @@
+package com.coyoapp.tinytask.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Table(name = "app_users")
+@Entity
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class AppUser {
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "id", nullable = false, updatable = false)
+  private String id;
+
+  private String name;
+
+  @Email
+  @Column(unique = true)
+  private String email;
+
+}
