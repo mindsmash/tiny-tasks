73aa6eaec058fde7cb6177c1ddad83245c5ff2b8
khallad2 <khallad_3@hotmail.com>
2019-09-08 18:40:29 +0200

    fix:migration

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,4 +1,4 @@
-create TABLE task (
+CREATE TABLE task (
     id VARCHAR(36) CONSTRAINT task_id_pkey PRIMARY KEY,
     name VARCHAR (128) NOT NULL,
     created TIMESTAMP WITH TIME ZONE NOT NULL
--- src/main/resources/db/migration/V2__ADD_TASK_STATUS.sql
+++
@@ -1,1 +0,0 @@
-alter TABLE task ADD status varchar(128) DEFAULT 'PENDING';
