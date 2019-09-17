064e0be3003bccab8189222b2a87471266077fe1
Jules <fehr.jules@gmail.com>
2019-09-17 21:33:13 +0200

    [Add] UserDetailsServiceImpl and BCryptPasswordEncoder

--- src/main/java/com/coyoapp/tinytask/security/WebSecurityConfig.java
+++ src/main/java/com/coyoapp/tinytask/security/WebSecurityConfig.java
@@ -1,19 +1,31 @@
 package com.coyoapp.tinytask.security;
 
+import com.coyoapp.tinytask.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder;
+  }
+
   @Override
   protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("jules").password("test").roles("ADMIN");
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
   }
 
   @Override
---
+++ src/main/java/com/coyoapp/tinytask/service/UserDetailsServiceImpl.java
@@ -0,0 +1,16 @@
+package com.coyoapp.tinytask.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  @Override
+  public UserDetails loadUserByUsername(
+    String username) throws UsernameNotFoundException {
+    return null;
+  }
+}
