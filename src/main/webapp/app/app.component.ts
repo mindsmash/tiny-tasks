97a66951167669a46a532a6632dd77d01e634e02
Jules <fehr.jules@gmail.com>
2019-09-21 01:04:52 +0200

    [Change] imperatively redirect until auth guard works

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,18 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "app/login/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  private model: any = {};
-
-  constructor() {
-  }
-
-  public ngOnInit(): void {
+  constructor(authService: AuthService, router: Router) {
+    if (authService.isLoggedIn()) {
+      router.navigate(['/tasks']);
+    }
+    router.navigate(['/login']);
   }
 
 }
