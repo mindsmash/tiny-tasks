4467d43fcfdeac25c963c9dd0c2620f0f7737d16
Fynn Feldpausch <fynn.feldpausch@coyoapp.com>
2019-10-07 16:14:57 +0200

    Updated cypress tests

--- src/main/cypress/integration/app.spec.ts
+++ src/main/cypress/integration/app.spec.ts
@@ -4,11 +4,4 @@ context('App', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-
-  it('should add a task', () => {
-    cy.get('[data-cy=task-input]')
-      .type('My task{enter}');
-    cy.get('[data-cy=task-list]')
-      .contains('My task');
-  });
 });
