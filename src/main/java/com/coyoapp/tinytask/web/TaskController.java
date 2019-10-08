37a7dc2c8b568ff8db33d3eff3f24fc56cf72df6
Nestor Segura <nestor.segura@gmai.com>
2019-10-08 23:27:14 +0200

    add controller with test

--- src/main/java/com/coyoapp/tinytask/web/TaskController.java
+++ src/main/java/com/coyoapp/tinytask/web/TaskController.java
@@ -3,19 +3,13 @@ package com.coyoapp.tinytask.web;
 import com.coyoapp.tinytask.dto.TaskRequest;
 import com.coyoapp.tinytask.dto.TaskResponse;
 import com.coyoapp.tinytask.service.TaskService;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Slf4j
@@ -43,5 +37,12 @@ public class TaskController {
   public void deleteTask(@PathVariable String taskId) {
     log.debug("deleteTask(taskId={})", taskId);
     taskService.deleteTask(taskId);
+  }
+
+  @ResponseStatus(HttpStatus.OK)
+  @PostMapping("/marktaskdone/{taskId}")
+  public void markTaskDone(@PathVariable String taskId) {
+    log.debug("markTaskDone(taskId={})", taskId);
+    taskService.markTaskDone(taskId);
   }
 }
--- src/test/java/com/coyoapp/tinytask/web/TaskControllerTest.java
+++ src/test/java/com/coyoapp/tinytask/web/TaskControllerTest.java
@@ -3,24 +3,17 @@ package com.coyoapp.tinytask.web;
 import com.coyoapp.tinytask.dto.TaskRequest;
 import com.coyoapp.tinytask.dto.TaskResponse;
 import com.coyoapp.tinytask.exception.TaskNotFoundException;
-import java.util.Collections;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class TaskControllerTest extends BaseControllerTest {
 
@@ -97,6 +90,31 @@ public class TaskControllerTest extends BaseControllerTest {
     ResultActions actualResult = this.mockMvc.perform(delete(PATH + "/" + id));
 
     // then
+    actualResult
+      .andDo(print())
+      .andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void shouldMarkTaskDone() throws Exception {
+    String id = "task-id";
+
+    ResultActions actualResult = this.mockMvc.perform(post(PATH + "/marktaskdone/" + id));
+
+    actualResult
+      .andDo(print())
+      .andExpect(status().isOk());
+
+    verify(taskService).markTaskDone(id);
+  }
+
+  @Test
+  public void shouldNotUpdateTask() throws Exception {
+    String id = "unknown-task-id";
+    doThrow(new TaskNotFoundException()).when(taskService).markTaskDone(id);
+
+    ResultActions actualResult = this.mockMvc.perform(post(PATH + "/marktaskdone/" + id));
+
     actualResult
       .andDo(print())
       .andExpect(status().isNotFound());
