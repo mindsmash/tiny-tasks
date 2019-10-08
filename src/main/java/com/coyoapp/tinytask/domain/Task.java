54c162f33d2f5328c4d36e56563965a5a51b59e3
Nestor Segura <nestor.segura@gmai.com>
2019-10-08 23:13:49 +0200

    add isDone variable to Entity and impl migration

--- src/main/java/com/coyoapp/tinytask/domain/Task.java
+++ src/main/java/com/coyoapp/tinytask/domain/Task.java
@@ -1,17 +1,13 @@
 package com.coyoapp.tinytask.domain;
 
-import java.time.Instant;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.Instant;
 
 @Table(name = "task")
 @Entity
@@ -30,4 +26,7 @@ public class Task {
 
   @CreatedDate
   private Instant created;
+
+  private boolean isDone;
+
 }
