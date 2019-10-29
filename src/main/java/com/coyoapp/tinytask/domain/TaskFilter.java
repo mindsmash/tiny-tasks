49c96587355eb734df7183549a3329b39cd2ea30
Oleksii <dana.chudnov@gmail.com>
2019-10-29 11:30:59 +0100

    init commit

--- src/main/java/com/coyoapp/tinytask/domain/TaskFilter.java
+++ src/main/java/com/coyoapp/tinytask/domain/TaskFilter.java
@@ -2,15 +2,18 @@ package com.coyoapp.tinytask.domain;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import static java.lang.String.format;
 import static org.springframework.data.jpa.domain.Specification.where;
 
 public class TaskFilter {
+
+  private static final String BASE_QUERY = "%%s%";
 
   public static Specification<Task> getNameFilter(String name) {
     return (root, query, cb) -> where(contains(name)).toPredicate(root, query, cb);
   }
 
   private static Specification<Task> contains(String value) {
-    return (root, query, cb) ->  cb.like(cb.lower(root.get("name")), "%" + value.toLowerCase() + "%");
+    return (root, query, cb) ->  cb.like(cb.lower(root.get("name")), format(BASE_QUERY, value.toLowerCase()));
   }
 }
