e660a7d00b7d9a044383822d53fee212a82b850e
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-01-25 19:11:07 +0200

    Adding the HTML for the date and time

--- src/main/webapp/app/tasks/tasks.module.ts
+++ src/main/webapp/app/tasks/tasks.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
@@ -17,7 +18,9 @@ import { TaskListComponent } from './task-list/task-list.component';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [TaskFormComponent, TaskListComponent]
 })
