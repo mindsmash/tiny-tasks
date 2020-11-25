7b4adbe4f3c2132899cec7632fd251323f455816
Patrik Kühl <patrik.kuehl@mailbox.org>
2020-11-25 20:29:56 +0100

    Implemented the User model.

---
+++ src/main/java/com/coyoapp/tinytask/domain/User.java
@@ -0,0 +1,22 @@
+package com.coyoapp.tinytask.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+  @Id
+  private String username;
+  private String password;
+}
