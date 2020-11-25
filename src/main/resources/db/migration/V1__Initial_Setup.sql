545366d2b61881fde52146f5bb3ad5765fea68b1
Patrik Kühl <patrik.kuehl@mailbox.org>
2020-11-25 20:21:26 +0100

    Made username and creator follow the maximum length of 24 characters.

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TABLE task (
     id VARCHAR(36) CONSTRAINT task_id_pkey PRIMARY KEY,
     name VARCHAR (128) NOT NULL,
-    creator VARCHAR (128) NOT NULL,
+    creator VARCHAR (24) NOT NULL,
     created TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
