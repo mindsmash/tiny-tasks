1255cbffd984c8fe5370c998b63e5a47fd7e3e49
Patrik Kühl <patrik.kuehl@mailbox.org>
2020-11-27 09:53:42 +0100

    Removed unused imports.

--- src/main/java/com/coyoapp/tinytask/web/TaskController.java
+++ src/main/java/com/coyoapp/tinytask/web/TaskController.java
@@ -3,18 +3,15 @@ package com.coyoapp.tinytask.web;
 import com.coyoapp.tinytask.dto.TaskRequest;
 import com.coyoapp.tinytask.dto.TaskResponse;
 import com.coyoapp.tinytask.service.DefaultTaskService;
-import com.coyoapp.tinytask.service.TaskService;
 
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
