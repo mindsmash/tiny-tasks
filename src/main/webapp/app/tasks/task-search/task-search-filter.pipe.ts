087a94f9d0da056a4c09234e14ca3a609811b11e
Leonard Schmischke <LS@roconcept.de>
2021-06-08 16:55:45 +0200

    Added support for AND and OR search operators

--- src/main/webapp/app/tasks/task-search/task-search-filter.pipe.ts
+++ src/main/webapp/app/tasks/task-search/task-search-filter.pipe.ts
@@ -13,6 +13,25 @@ export class TaskSearchFilterPipe implements PipeTransform {
     if (key === null || key.length === 0 || tasks === null) {
       return tasks;
     }
-    return tasks.filter(task => task.name.includes(key));
+    // advanced search logic with search operators - AND and OR implemented
+
+    // OR has lower precedence than AND, so search first for OR (parsing from outside to inside)
+    const orQueries = key.split(new RegExp('OR')).map(item => item.trim()).filter(item => item.length > 0);
+    console.log(orQueries);
+    const orResults = orQueries.map (orQuery => {
+      // get AND queries
+      const andQueries = orQuery.split(new RegExp('AND')).map(item => item.trim());
+
+      // get results of individual queries
+      const andResults = andQueries.map(andQuery => {
+        return tasks.filter(task => task.name.toLocaleLowerCase().includes(andQuery));
+      });
+
+      // find intersection of results (AND logic)
+      return andResults.reduce((intersection, singleResultList) => intersection.filter(item => singleResultList.includes(item)));
+    });
+    // combine and return results of OR queries
+    return orResults.reduce((union, singleResultList) => [...new Set([...union, ...singleResultList])]);
+
   }
 }
