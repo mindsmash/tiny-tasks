0eec49bdb8d9051f9d9332956862cce24a62dbb4
BKR <booker@pureops.tech>
2021-06-13 23:34:39 +0200

    added roles migration so users have roles

--- src/main/java/com/coyoapp/tinytask/dto/UserResponse.java
+++ src/main/java/com/coyoapp/tinytask/dto/UserResponse.java
@@ -1,9 +1,14 @@
 package com.coyoapp.tinytask.dto;
 
+import com.coyoapp.tinytask.domain.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -12,7 +17,12 @@ import lombok.NoArgsConstructor;
 public class UserResponse {
 
   private String id;
-
+  private Set<Role> roles = new HashSet<>();
   private String username;
+  private String firstname;
+  private String lastname;
+  private String email;
+  private Instant created;
+  private Instant modified;
 
 }
