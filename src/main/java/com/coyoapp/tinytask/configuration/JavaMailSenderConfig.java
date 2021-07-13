cb25a16a7327e13ba628932ea26ef9987d12821d
Jonathan Heyer <54237352+not-enough-ram@users.noreply.github.com>
2021-07-13 08:42:15 +0200

    Setting up gradle and JavaMail, enabled scheduling

---
+++ src/main/java/com/coyoapp/tinytask/configuration/JavaMailSenderConfig.java
@@ -0,0 +1,27 @@
+package com.coyoapp.tinytask.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class JavaMailSenderConfig {
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
+
+    mailSender.setUsername("foobar");
+    mailSender.setPassword("some password");
+
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
+    return mailSender;
+  }
+}
