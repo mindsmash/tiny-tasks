e3f2ec3cf5ed5f8420dc8847b91753a430bf8b9d
not-enough-ram <jonathan.heyer@gmail.com>
2021-07-15 09:24:24 +0200

    fixed test

--- src/test/java/com/coyoapp/tinytask/service/MailServiceTest.java
+++ src/test/java/com/coyoapp/tinytask/service/MailServiceTest.java
@@ -43,31 +43,58 @@ class MailServiceTest {
         .id("task id")
         .created(Instant.now())
         .name("Open Task")
-        .build()));
+        .build(),
+        Task.builder()
+          .inProgress(true)
+          .id("task id 2")
+          .created(Instant.now())
+          .name("Another Open Task")
+          .build(),
+        Task.builder()
+          .inProgress(false)
+          .id("task id 3")
+          .created(Instant.now())
+          .name("This is done")
+          .build()));
 
     when(taskRepository.findAllByUserId("another id"))
       .thenReturn(List.of(Task.builder()
         .inProgress(false)
-        .id("task id")
+        .id("task id 4")
         .created(Instant.now())
         .name("Done Task")
-        .build()));
+        .build(),
+        Task.builder()
+          .inProgress(true)
+          .id("task id 5")
+          .created(Instant.now())
+          .name("Oh my, it's an open Task")
+          .build()));
 
     SimpleMailMessage mailMessageFoobar = new SimpleMailMessage();
     mailMessageFoobar.setFrom("noreply@yourdailytasks.io");
     mailMessageFoobar.setTo("some@mail.com");
     mailMessageFoobar.setSubject("Hey! Here are some tasks for you :)");
     String mailFoobar = "Good morning " + "Foobar" + " here is a list of your open tasks: " +
-      "\n" + "Open Task";
+      "\n" + "Open Task" + "\n" + "Another Open Task";
     mailMessageFoobar.setText(mailFoobar);
+
+    SimpleMailMessage mailMessageBaz = new SimpleMailMessage();
+    mailMessageBaz.setFrom("noreply@yourdailytasks.io");
+    mailMessageBaz.setTo("another@mail.com");
+    mailMessageBaz.setSubject("Hey! Here are some tasks for you :)");
+    String mailBaz = "Good morning " + "Baz" + " here is a list of your open tasks: " +
+      "\n" + "Oh my, it's an open Task";
+    mailMessageBaz.setText(mailBaz);
 
     //WHEN
     mailService.sendMail();
 
     //THEN
     verify(userRepository, times(1)).findAll();
-    verify(taskRepository, times(1)).findAllByUserId("Foobar");
-    verify(taskRepository, times(1)).findAllByUserId("Baz");
+    verify(taskRepository, times(1)).findAllByUserId("some id");
+    verify(taskRepository, times(1)).findAllByUserId("another id");
     verify(javaMailSender, times(1)).send(mailMessageFoobar);
+    verify(javaMailSender, times(1)).send(mailMessageBaz);
   }
 }
