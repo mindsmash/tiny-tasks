19a10ee5d41cd565493ad81520956c423e61311d
Rebekka <re.schaub@web.de>
2021-08-03 02:47:58 +0200

    Add MailConfig

--- src/main/java/com/coyoapp/tinytask/configuration/MailConfig.java
+++ src/main/java/com/coyoapp/tinytask/configuration/MailConfig.java
@@ -1,15 +1,38 @@
 package com.coyoapp.tinytask.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@Getter
+@Setter
 public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
 
   @Bean
   public JavaMailSender createMailSender(){
-    return new JavaMailSenderImpl();
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(getHost());
+    mailSender.setPort(getPort());
+    mailSender.setUsername(getUsername());
+    mailSender.setPassword(getPassword());
+
+    return mailSender;
   }
 }
