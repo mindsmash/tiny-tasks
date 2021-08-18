f958de5f376681bcf7d9d678ddec12565e7a4267
Mohammad Jalili <mohijalili@icloud.com>
2021-08-19 00:55:12 +0800

    feat(app): register comp/pipe in module

--- src/main/webapp/app/tasks/tasks.module.ts
+++ src/main/webapp/app/tasks/tasks.module.ts
@@ -5,19 +5,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
+import { SortByPipe } from 'app/pipes/sort-by.pipe';
+
 @NgModule({
-  declarations: [TaskFormComponent, TaskListComponent],
+  declarations: [TaskFormComponent, TaskListComponent, SortByPipe],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   exports: [TaskFormComponent, TaskListComponent]
 })
