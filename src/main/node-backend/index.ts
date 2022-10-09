ff7b96105a0ecc8b9025ea838ca5062c935146e3
Andrei Colta <andrei.colta@bearingpoint.com>
2022-10-09 22:34:57 +0300

    feat: add status functionality to backend

--- src/main/node-backend/index.ts
+++ src/main/node-backend/index.ts
@@ -1,14 +1,35 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-import { Task } from './models/task';
+import { Status, Task } from './models/task';
 
 const port = 8080;
 
 const app: Express = express();
-const tasks: Task[] = [];
-let idCount = 0;
+let tasks: Task[] = [
+  {
+    id: '0',
+    name: 'New Task 1',
+    status: Status.NEW,
+  },
+  {
+    id: '1',
+    name: 'New Task 2',
+    status: Status.IN_PROGRESS,
+  },
+  {
+    id: '2',
+    name: 'New Task 3',
+    status: Status.BLOCKED,
+  },
+  {
+    id: '3',
+    name: 'New Task 4',
+    status: Status.DONE,
+  },
+];
+let idCount = 4;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,14 +46,17 @@ app.get('/tasks', (req: any, res: any) => {
     });
   }
 
+  resultTasks = getSortedTasksByStatus(resultTasks);
+
   res.send(resultTasks);
 });
 
 app.post('/tasks', (req: any, res: any) => {
   const name = req.body.name;
   const newTask: Task = {
-    id: '' + idCount,
+    id: `${idCount}`,
     name,
+    status: Status.NEW,
   };
 
   idCount++;
@@ -40,6 +64,20 @@ app.post('/tasks', (req: any, res: any) => {
   tasks.push(newTask);
 
   res.send(newTask);
+});
+
+app.patch('/tasks', (req: any, res: any) => {
+  const updatedTask = req.body.task;
+  const index = tasks.findIndex((task) => task.id === updatedTask.id);
+
+  tasks[index] = updatedTask;
+
+  res.status(200).send();
+});
+
+app.delete('/tasks/clear-done', (req: any, res: any) => {
+  tasks = tasks.filter((task) => task.status !== Status.DONE);
+  res.status(200).send();
 });
 
 app.delete('/tasks/:id', (req: any, res: any) => {
@@ -55,3 +93,15 @@ app.delete('/tasks/:id', (req: any, res: any) => {
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
+
+// HELPERS
+
+const getSortedTasksByStatus = (tasks: Task[]) =>
+  tasks.sort((a: Task, b: Task) => {
+    if (a.status === Status.DONE && b.status !== Status.DONE) {
+      return 1;
+    } else if (a.status !== Status.DONE && b.status === Status.DONE) {
+      return -1;
+    }
+    return 0;
+  });
--- src/main/node-backend/models/task.ts
+++ src/main/node-backend/models/task.ts
@@ -1,7 +1,12 @@
-/**
- * A tiny task.
- */
+export enum Status {
+  NEW = 'NEW',
+  IN_PROGRESS = 'IN_PROGRESS',
+  BLOCKED = 'BLOCKED',
+  DONE = 'DONE',
+}
+
 export interface Task {
   id: string;
   name: string;
+  status: Status;
 }
