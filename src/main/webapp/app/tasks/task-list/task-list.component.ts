c61767a985abfba3331d17bf33c9826a515efcfe
Vitor Batista <vitormlb@hotmail.com>
2022-10-20 22:26:44 +0100

    Add KanbanBoard to the view and manage state of Tasks

--- src/main/webapp/app/tasks/task-list/task-list.component.ts
+++ src/main/webapp/app/tasks/task-list/task-list.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ChangeDetectionStrategy, Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { TaskState } from 'src/main/webapp/app/enum/task-state.enum';
+import { LocalTaskService } from 'src/main/webapp/app/tasks/local-task.service';
 
-import { Task } from '../task';
+import { Task } from '../../model/task';
 import { TaskService } from '../task.service';
 
 /**
@@ -12,17 +15,70 @@ import { TaskService } from '../task.service';
   styleUrls: ['./task-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
 
   @Input() tasks: Task[] | null = null;
 
   @Output() deleted: EventEmitter<Task> = new EventEmitter();
 
-  constructor(@Inject('TaskService') private taskService: TaskService) {}
+  toDoTasks: Task[] = [];
+  inProgressTasks: Task[] = [];
+  blockedTasks: Task[] = [];
+  doneTasks: Task[] = [];
+
+  constructor(@Inject('TaskService') private taskService: TaskService, private localTaskService: LocalTaskService) { }
+
+  ngOnInit(): void {
+    this.filterTasksByState();
+  }
 
   delete(task: Task): void {
     this.taskService.delete(task.id).subscribe(() => {
       this.deleted.emit(task);
+    });
+  }
+
+  filterTasksByState(): void {
+    if (this.tasks) {
+      this.toDoTasks = this.tasks.filter(task => task.state === TaskState.TODO || !task.state);
+      this.inProgressTasks = this.tasks.filter(task => task.state === TaskState.IN_PROGRESS);
+      this.blockedTasks = this.tasks.filter(task => task.state === TaskState.BLOCKED);
+      this.doneTasks = this.tasks.filter(task => task.state === TaskState.DONE);
+    }
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+      const task = event.container.data[event.currentIndex];
+      task.state = this.getStateByContainerId(event.container.id);
+      this.localTaskService.update(task).subscribe();
+    } else {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+
+  getStateByContainerId(containerId: string): TaskState {
+    switch (containerId) {
+      case 'cdk-drop-list-0':
+        return TaskState.TODO;
+      case 'cdk-drop-list-1':
+        return TaskState.IN_PROGRESS;
+      case 'cdk-drop-list-2':
+        return TaskState.BLOCKED;
+      case 'cdk-drop-list-3':
+        return TaskState.DONE;
+    }
+    return TaskState.TODO;
+  }
+
+  updateTaskState(task: Task): void {
+    debugger
+    this.localTaskService.update(task).subscribe(() => {
+      this.filterTasksByState();
     });
   }
 }
