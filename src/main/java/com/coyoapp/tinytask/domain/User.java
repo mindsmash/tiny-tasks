7a37f37e2fea4b85a98b7f66cca5aa8fba7b7e35
buikhoa793@gmail.com <buikhoa793@gmail.com>
2024-06-09 18:11:28 +0300

    Add user table and update task table

---
+++ src/main/java/com/coyoapp/tinytask/domain/User.java
@@ -0,0 +1,32 @@
+package com.coyoapp.tinytask.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "user") // Use double quotes to handle reserved keywords
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 50)
+  private String email;
+
+  @Column(nullable = false, length = 50)
+  private String password;
+
+  @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime created;
+
+  @OneToMany(mappedBy = "user")
+  private List<Task> tasks;
+
+}
--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,15 +1,18 @@
-CREATE TABLE task (
-    id VARCHAR(36) CONSTRAINT task_id_pkey PRIMARY KEY,
-    name VARCHAR (128) NOT NULL,
-    created TIMESTAMP WITH TIME ZONE NOT NULL
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+CREATE TABLE "user"
+(
+  id       BIGSERIAL PRIMARY KEY,
+  email    VARCHAR(128) NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  created  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user
+CREATE TABLE task
 (
-  id       SERIAL PRIMARY KEY,
-  username VARCHAR(128) NOT NULL,
-  password VARCHAR(128) NOT NULL,
-  created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  id      VARCHAR(36)
+    CONSTRAINT task_id_pkey PRIMARY KEY,
+  name    VARCHAR(128)             NOT NULL,
+  created TIMESTAMP WITH TIME ZONE NOT NULL,
+  user_id BIGINT                  NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
+
