8d567ae2390c538f1a823195e268fcccd1f641e8
buikhoa793@gmail.com <buikhoa793@gmail.com>
2024-06-10 20:07:23 +0300

    Fix component name

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tiny-app',
-  standalone: true,
-  imports: [],
+  selector: 'tiny-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/main/webapp/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/login', component: LoginComponent },
+];
