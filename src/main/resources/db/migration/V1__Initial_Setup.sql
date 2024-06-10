d0d787596c98584038ca051965533d15e366f320
buikhoa793@gmail.com <buikhoa793@gmail.com>
2024-06-11 01:01:43 +0300

    Fix DB migration script

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,18 +1,5 @@
-CREATE TABLE "user"
-(
-  id       BIGSERIAL PRIMARY KEY,
-  email    VARCHAR(128) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  created  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE task (
+    id VARCHAR(36) CONSTRAINT task_id_pkey PRIMARY KEY,
+    name VARCHAR (128) NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL
 );
-
-CREATE TABLE task
-(
-  id      VARCHAR(36)
-    CONSTRAINT task_id_pkey PRIMARY KEY,
-  name    VARCHAR(128)             NOT NULL,
-  created TIMESTAMP WITH TIME ZONE NOT NULL,
-  user_id BIGINT                  NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES "user" (id)
-);
-
