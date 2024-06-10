125061014a9c8caedb7712ea387b51b769a10b45
buikhoa793@gmail.com <buikhoa793@gmail.com>
2024-06-10 20:18:37 +0300

    Screens navigation

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -4,15 +4,18 @@ import { environment } from '../environments/environment';
 import { BASE_URL } from './app.tokens';
 import { DefaultTaskService } from './tasks/default-task.service';
 import { LocalTaskService } from './tasks/local-task.service';
-import { TasksModule } from './tasks/tasks.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.modue';
 import { LoginComponent } from './login/login.component';
 
+import { RouterLink, RouterOutlet, provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [HomeModule, LoginComponent],
+  imports: [RouterOutlet, RouterLink, HomeModule, LoginComponent],
   providers: [
+    provideRouter(routes),
     { provide: BASE_URL, useValue: 'http://localhost:8080' },
     {
       provide: 'TaskService',
--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -2,6 +2,6 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes = [
-  { path: '/', component: HomeComponent },
-  { path: '/login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
 ];
