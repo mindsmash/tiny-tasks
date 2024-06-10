d732b6bedf67ff8db022adacad6c29755d7da7db
buikhoa793@gmail.com <buikhoa793@gmail.com>
2024-06-10 20:58:31 +0300

    Login form and login handler

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -4,4 +4,5 @@ import { LoginComponent } from './login/login.component';
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login as default
 ];
--- src/main/webapp/app/login/login.component.ts
+++ src/main/webapp/app/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'tiny-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      // Implement authentication logic here
+      console.log('Email:', email);
+      console.log('Password:', password);
+    }
+  }
 }
